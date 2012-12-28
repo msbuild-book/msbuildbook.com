ef8b831d9aeab5e9ba928b45e2293827afd4ab4e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-12-28 12:08:09 -0800

    adding tools

---
+++ prep-for-publish.ps1
@@ -0,0 +1,28 @@
+﻿
+
+
+# To be continued
+
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+function CompressCss 
+{
+    param($packerPath,$scriptDir)
+    if(!(Test-Path $packerPath)){
+        throw ("packer.exe not found at $packerPath")
+    }
+
+
+    $command = ("'{0}' -o " -f $packerPath)
+
+}
+
+$scriptDir = Get-ScriptDirectory 
+$toolsDir = Join-Path -Path $scriptDir -ChildPath tools
+$packerPath = Join-Path -Path $toolsDir -ChildPath packer\packer.exe
+
+"Scripts directory $scriptDir" | Write-Host
