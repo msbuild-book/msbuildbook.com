f9a4a47dd489d6f3773fbfde0a6b98e3c6c04772
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-12-31 00:25:40 -0800

    updating microdata

--- minify.ps1
+++ minify.ps1
@@ -3,7 +3,7 @@
 if((Test-Path $ajaxMinPath)){
     Set-Alias ajaxmin $ajaxMinPath
 
-    ajaxmin -CSS .\WebSite\content\msb.css .\WebSite\content\msb.min.css
+    ajaxmin -CSS .\WebSite\content\msb.css -out .\WebSite\content\msb.min.css
 
     ajaxmin -JS .\WebSite\scripts\allScripts.js -out .\WebSite\scripts\allScripts.min.js
 }
