c26944932fb48e400fa68dabf6cc77df99ac8061
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-12-30 17:46:07 -0800

    optimizing site

---
+++ minify.ps1
@@ -0,0 +1,12 @@
+﻿$ajaxMinPath = 'C:\Program Files (x86)\Microsoft\Microsoft Ajax Minifier\AjaxMin.exe'
+
+if((Test-Path $ajaxMinPath)){
+    Set-Alias ajaxmin $ajaxMinPath
+
+    ajaxmin -CSS .\WebSite\content\msb.css .\WebSite\content\msb.min.css
+
+    ajaxmin -JS .\WebSite\scripts\allScripts.js -out .\WebSite\scripts\allScripts.min.js
+}
+else {
+    throw "Ajax Min not found at [$ajaxMinPath]"
+}
